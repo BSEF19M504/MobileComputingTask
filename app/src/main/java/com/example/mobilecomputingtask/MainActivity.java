dd6f331d9c042141a329cc05e6bf79bcc15d6749
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-04-19 14:43:10 +0500

    Added buttons to change contents of text view

--- app/src/main/java/com/example/mobilecomputingtask/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingtask/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.mobilecomputingtask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button firstButton;
+    Button secondButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstButton = findViewById(R.id.firstButton);
+        secondButton = findViewById(R.id.secondButton);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    public void changeFirst(View view) {
+        textView.setText("First Button Click");
+    }
+
+    public void changeSecond(View view) {
+        textView.setText("Second Button Click");
     }
 }
