65f083fa3ac85dc7630bc4ae3ae0719122a38275
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-05-18 22:09:04 +0500

    Added image resources and mapped them to button clicks in new activity

--- app/src/main/java/com/example/mobilecomputingtask/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingtask/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mobilecomputingtask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,44 +12,130 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView textView;
-    EditText editText;
-    Button firstButton;
-    Button secondButton;
+    Button [] alphabets;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstButton = findViewById(R.id.firstButton);
-        secondButton = findViewById(R.id.secondButton);
+        alphabets = new Button[26];
 
-        firstButton.setOnClickListener(this);
+        alphabets[0] = findViewById(R.id.button);
+        alphabets[1] = findViewById(R.id.button2);
+        alphabets[2] = findViewById(R.id.button3);
+        alphabets[3] = findViewById(R.id.button4);
+        alphabets[4] = findViewById(R.id.button5);
+        alphabets[5] = findViewById(R.id.button6);
+        alphabets[6] = findViewById(R.id.button7);
+        alphabets[7] = findViewById(R.id.button8);
+        alphabets[8] = findViewById(R.id.button9);
+        alphabets[9] = findViewById(R.id.button10);
+        alphabets[10] = findViewById(R.id.button11);
+        alphabets[11] = findViewById(R.id.button12);
+        alphabets[12] = findViewById(R.id.button13);
+        alphabets[13] = findViewById(R.id.button14);
+        alphabets[14] = findViewById(R.id.button15);
+        alphabets[15] = findViewById(R.id.button16);
+        alphabets[16] = findViewById(R.id.button17);
+        alphabets[17] = findViewById(R.id.button18);
+        alphabets[18] = findViewById(R.id.button19);
+        alphabets[19] = findViewById(R.id.button20);
+        alphabets[20] = findViewById(R.id.button21);
+        alphabets[21] = findViewById(R.id.button22);
+        alphabets[22] = findViewById(R.id.button23);
+        alphabets[23] = findViewById(R.id.button24);
+        alphabets[24] = findViewById(R.id.button25);
+        alphabets[25] = findViewById(R.id.button26);
 
-        secondButton.setOnClickListener(this);
-
-        editText = findViewById(R.id.editTextTextPersonName3);
-        textView = findViewById(R.id.textView);
+        for(int i=0; i<26; i++){
+            alphabets[i].setOnClickListener(this);
+        }
     }
 
-    private void changeFirst() {
-        textView.setText(editText.getText().toString());
-    }
-
-    private void changeSecond() {
-        textView.setText("");
-        editText.setText("");
-    }
 
     @Override
     public void onClick(View view) {
+        Intent intent = new Intent(this,MainActivity2.class);
         switch (view.getId()){
-            case R.id.firstButton:
-                changeFirst();
+            case R.id.button:
+                intent.putExtra("char","a");
                 break;
-            case R.id.secondButton:
-                changeSecond();
+            case R.id.button2:
+                intent.putExtra("char","b");
+                break;
+            case R.id.button3:
+                intent.putExtra("char","c");
+                break;
+            case R.id.button4:
+                intent.putExtra("char","d");
+                break;
+            case R.id.button5:
+                intent.putExtra("char","e");
+                break;
+            case R.id.button6:
+                intent.putExtra("char","f");
+                break;
+            case R.id.button7:
+                intent.putExtra("char","g");
+                break;
+            case R.id.button8:
+                intent.putExtra("char","h");
+                break;
+            case R.id.button9:
+                intent.putExtra("char","i");
+                break;
+            case R.id.button10:
+                intent.putExtra("char","j");
+                break;
+            case R.id.button11:
+                intent.putExtra("char","k");
+                break;
+            case R.id.button12:
+                intent.putExtra("char","l");
+                break;
+            case R.id.button13:
+                intent.putExtra("char","m");
+                break;
+            case R.id.button14:
+                intent.putExtra("char","n");
+                break;
+            case R.id.button15:
+                intent.putExtra("char","o");
+                break;
+            case R.id.button16:
+                intent.putExtra("char","p");
+                break;
+            case R.id.button17:
+                intent.putExtra("char","q");
+                break;
+            case R.id.button18:
+                intent.putExtra("char","r");
+                break;
+            case R.id.button19:
+                intent.putExtra("char","s");
+                break;
+            case R.id.button20:
+                intent.putExtra("char","t");
+                break;
+            case R.id.button21:
+                intent.putExtra("char","u");
+                break;
+            case R.id.button22:
+                intent.putExtra("char","v");
+                break;
+            case R.id.button23:
+                intent.putExtra("char","w");
+                break;
+            case R.id.button24:
+                intent.putExtra("char","x");
+                break;
+            case R.id.button25:
+                intent.putExtra("char","y");
+                break;
+            case R.id.button26:
+                intent.putExtra("char","z");
                 break;
         }
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mobilecomputingtask/MainActivity2.java
@@ -0,0 +1,161 @@
+package com.example.mobilecomputingtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ImageView imageView, imageView1, imageView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String entry = intent.getStringExtra("char");
+        char alphabet = entry.charAt(0);
+        imageView = findViewById(R.id.imageView);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView.setImageResource(R.drawable.a1);
+        imageView1.setImageResource(R.drawable.a2);
+        imageView2.setImageResource(R.drawable.a3);
+
+        switch (alphabet){
+            case 'a':
+                imageView.setImageResource(R.drawable.a1);
+                imageView1.setImageResource(R.drawable.a2);
+                imageView2.setImageResource(R.drawable.a3);
+                break;
+            case 'b':
+                imageView.setImageResource(R.drawable.b1);
+                imageView1.setImageResource(R.drawable.b2);
+                imageView2.setImageResource(R.drawable.b3);
+                break;
+            case 'c':
+                imageView.setImageResource(R.drawable.c1);
+                imageView1.setImageResource(R.drawable.c2);
+                imageView2.setImageResource(R.drawable.c3);
+                break;
+            case 'd':
+                imageView.setImageResource(R.drawable.d1);
+                imageView1.setImageResource(R.drawable.d2);
+                imageView2.setImageResource(R.drawable.d3);
+                break;
+            case 'e':
+                imageView.setImageResource(R.drawable.e1);
+                imageView1.setImageResource(R.drawable.e2);
+                imageView2.setImageResource(R.drawable.e3);
+                break;
+            case 'f':
+                imageView.setImageResource(R.drawable.f1);
+                imageView1.setImageResource(R.drawable.f2);
+                imageView2.setImageResource(R.drawable.f3);
+                break;
+            case 'g':
+                imageView.setImageResource(R.drawable.g1);
+                imageView1.setImageResource(R.drawable.g2);
+                imageView2.setImageResource(R.drawable.g3);
+                break;
+            case 'h':
+                imageView.setImageResource(R.drawable.h1);
+                imageView1.setImageResource(R.drawable.h2);
+                imageView2.setImageResource(R.drawable.h3);
+                break;
+            case 'i':
+                imageView.setImageResource(R.drawable.i1);
+                imageView1.setImageResource(R.drawable.i2);
+                imageView2.setImageResource(R.drawable.i3);
+                break;
+            case 'j':
+                imageView.setImageResource(R.drawable.j1);
+                imageView1.setImageResource(R.drawable.j2);
+                imageView2.setImageResource(R.drawable.j3);
+                break;
+            case 'k':
+                imageView.setImageResource(R.drawable.k1);
+                imageView1.setImageResource(R.drawable.k2);
+                imageView2.setImageResource(R.drawable.k3);
+                break;
+            case 'l':
+                imageView.setImageResource(R.drawable.l1);
+                imageView1.setImageResource(R.drawable.l2);
+                imageView2.setImageResource(R.drawable.l3);
+                break;
+            case 'm':
+                imageView.setImageResource(R.drawable.m1);
+                imageView1.setImageResource(R.drawable.m2);
+                imageView2.setImageResource(R.drawable.m3);
+                break;
+            case 'n':
+                imageView.setImageResource(R.drawable.n1);
+                imageView1.setImageResource(R.drawable.n2);
+                imageView2.setImageResource(R.drawable.n3);
+                break;
+            case 'o':
+                imageView.setImageResource(R.drawable.o1);
+                imageView1.setImageResource(R.drawable.o2);
+                imageView2.setImageResource(R.drawable.o3);
+                break;
+            case 'p':
+                imageView.setImageResource(R.drawable.p1);
+                imageView1.setImageResource(R.drawable.p2);
+                imageView2.setImageResource(R.drawable.p3);
+                break;
+            case 'q':
+                imageView.setImageResource(R.drawable.q1);
+                imageView1.setImageResource(R.drawable.q2);
+                imageView2.setImageResource(R.drawable.q3);
+                break;
+            case 'r':
+                imageView.setImageResource(R.drawable.r1);
+                imageView1.setImageResource(R.drawable.r2);
+                imageView2.setImageResource(R.drawable.r3);
+                break;
+            case 's':
+                imageView.setImageResource(R.drawable.s1);
+                imageView1.setImageResource(R.drawable.s2);
+                imageView2.setImageResource(R.drawable.s3);
+                break;
+            case 't':
+                imageView.setImageResource(R.drawable.t1);
+                imageView1.setImageResource(R.drawable.t2);
+                imageView2.setImageResource(R.drawable.t3);
+                break;
+            case 'u':
+                imageView.setImageResource(R.drawable.u1);
+                imageView1.setImageResource(R.drawable.u2);
+                imageView2.setImageResource(R.drawable.u3);
+                break;
+            case 'v':
+                imageView.setImageResource(R.drawable.v1);
+                imageView1.setImageResource(R.drawable.v2);
+                imageView2.setImageResource(R.drawable.v3);
+                break;
+            case 'w':
+                imageView.setImageResource(R.drawable.w1);
+                imageView1.setImageResource(R.drawable.w2);
+                imageView2.setImageResource(R.drawable.w3);
+                break;
+            case 'x':
+                imageView.setImageResource(R.drawable.x1);
+                imageView1.setImageResource(R.drawable.x2);
+                imageView2.setImageResource(R.drawable.x3);
+                break;
+            case 'y':
+                imageView.setImageResource(R.drawable.y1);
+                imageView1.setImageResource(R.drawable.y2);
+                imageView2.setImageResource(R.drawable.y3);
+                break;
+            case 'z':
+                imageView.setImageResource(R.drawable.z1);
+                imageView1.setImageResource(R.drawable.z2);
+                imageView2.setImageResource(R.drawable.z3);
+                break;
+        }
+
+    }
+}
