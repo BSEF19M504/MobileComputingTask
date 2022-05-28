ccb344c60cbc2ec584016f87a0d06f2be32eb0c3
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-05-28 19:27:04 +0500

    Removed hardcoded xml buttons and ScrollView and added ListView instead

--- app/src/main/java/com/example/mobilecomputingtask/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingtask/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         alphabets = new Button[26];
 
-        alphabets[0] = findViewById(R.id.button);
+        /*alphabets[0] = findViewById(R.id.button);
         alphabets[1] = findViewById(R.id.button2);
         alphabets[2] = findViewById(R.id.button3);
         alphabets[3] = findViewById(R.id.button4);
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         alphabets[22] = findViewById(R.id.button23);
         alphabets[23] = findViewById(R.id.button24);
         alphabets[24] = findViewById(R.id.button25);
-        alphabets[25] = findViewById(R.id.button26);
+        alphabets[25] = findViewById(R.id.button26);*/
 
         for(int i=0; i<26; i++){
             alphabets[i].setOnClickListener(this);
@@ -56,86 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         Intent intent = new Intent(this,MainActivity2.class);
-        switch (view.getId()){
-            case R.id.button:
-                intent.putExtra("char","a");
-                break;
-            case R.id.button2:
-                intent.putExtra("char","b");
-                break;
-            case R.id.button3:
-                intent.putExtra("char","c");
-                break;
-            case R.id.button4:
-                intent.putExtra("char","d");
-                break;
-            case R.id.button5:
-                intent.putExtra("char","e");
-                break;
-            case R.id.button6:
-                intent.putExtra("char","f");
-                break;
-            case R.id.button7:
-                intent.putExtra("char","g");
-                break;
-            case R.id.button8:
-                intent.putExtra("char","h");
-                break;
-            case R.id.button9:
-                intent.putExtra("char","i");
-                break;
-            case R.id.button10:
-                intent.putExtra("char","j");
-                break;
-            case R.id.button11:
-                intent.putExtra("char","k");
-                break;
-            case R.id.button12:
-                intent.putExtra("char","l");
-                break;
-            case R.id.button13:
-                intent.putExtra("char","m");
-                break;
-            case R.id.button14:
-                intent.putExtra("char","n");
-                break;
-            case R.id.button15:
-                intent.putExtra("char","o");
-                break;
-            case R.id.button16:
-                intent.putExtra("char","p");
-                break;
-            case R.id.button17:
-                intent.putExtra("char","q");
-                break;
-            case R.id.button18:
-                intent.putExtra("char","r");
-                break;
-            case R.id.button19:
-                intent.putExtra("char","s");
-                break;
-            case R.id.button20:
-                intent.putExtra("char","t");
-                break;
-            case R.id.button21:
-                intent.putExtra("char","u");
-                break;
-            case R.id.button22:
-                intent.putExtra("char","v");
-                break;
-            case R.id.button23:
-                intent.putExtra("char","w");
-                break;
-            case R.id.button24:
-                intent.putExtra("char","x");
-                break;
-            case R.id.button25:
-                intent.putExtra("char","y");
-                break;
-            case R.id.button26:
-                intent.putExtra("char","z");
-                break;
-        }
+
         startActivity(intent);
     }
 }
