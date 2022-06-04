13b4eb9ac0b626c34d872426ad273f057c86feb6
BSEF19M504 <bsef19m504@pucit.edu.pk>
2022-06-04 15:34:08 +0500

    Added new Launcher Activity with buttons for new and old app design

---
+++ app/src/main/java/com/example/mobilecomputingtask/IntroActivity.java
@@ -0,0 +1,45 @@
+package com.example.mobilecomputingtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button oldApp,newApp,repo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        oldApp = findViewById(R.id.old_layout);
+        newApp = findViewById(R.id.new_layout);
+        repo = findViewById(R.id.repo);
+
+        oldApp.setOnClickListener(this);
+        newApp.setOnClickListener(this);
+        repo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.old_layout:
+                intent = new Intent(this, OldIntroActivity.class);
+                break;
+            case R.id.new_layout:
+                intent = new Intent(this, OldQuizAnswerActivity.class);
+                break;
+            case R.id.repo:
+                Uri webpage = Uri.parse("https://github.com/BSEF19M504/MobileComputingTask");
+                intent = new Intent(Intent.ACTION_VIEW, webpage);
+                break;
+        }
+        startActivity(intent);
+    }
+}
